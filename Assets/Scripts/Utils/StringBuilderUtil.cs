3e37b6e4bf6c39e96c62e3afdaad77f1ac5a7b27
MythicalC <atito@annatito.com>
2019-07-25 17:15:08 +1000

    adding StringBuilderUtil
    StringBuilderUtil gives you a pool of string builders to rotate through so you can both avoid concatenating strings and avoid having to create a new StringBuilder every time you want to do more complex string creation.

---
+++ Assets/Scripts/Utils/StringBuilderUtil.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+public static class StringBuilderUtil
+{
+
+    private static StringBuilder _debugStringBuilder;
+    private static StringBuilder _errorStringBuilder;
+
+    private static List<StringBuilder> _stringBuilderPool;
+    private static int _maxPoolSize = 2;
+    private static int _activePoolItemIndex = 0;
+
+    public static StringBuilder GetEmptyStringBuilder()
+    {
+        if (_stringBuilderPool == null)
+        {
+            _stringBuilderPool = new List<StringBuilder>();
+        }
+
+        if (_stringBuilderPool.Count < _maxPoolSize)
+        {
+            StringBuilder sb = new StringBuilder();
+            _stringBuilderPool.Add(sb);
+            _activePoolItemIndex = _stringBuilderPool.Count - 1;
+            return _stringBuilderPool[_activePoolItemIndex];
+        }
+        else
+        {
+            _activePoolItemIndex = +1;
+            if (_activePoolItemIndex >= _maxPoolSize)
+            {
+                _activePoolItemIndex = 0;
+            }
+
+            _stringBuilderPool[_activePoolItemIndex].Length = 0;
+            return _stringBuilderPool[_activePoolItemIndex];
+        }
+    }
+
+    public static StringBuilder GetDebugStringBuilder()
+    {
+        if (_debugStringBuilder == null)
+        {
+            _debugStringBuilder = new StringBuilder();
+            return _debugStringBuilder;
+        }
+        else
+        {
+            _debugStringBuilder.Length = 0;
+            return _debugStringBuilder;
+        }
+    }
+
+    public static StringBuilder GetErrorStringBuilder()
+    {
+        if (_errorStringBuilder == null)
+        {
+            _errorStringBuilder = new StringBuilder();
+            return _errorStringBuilder;
+        }
+        else
+        {
+            _errorStringBuilder.Length = 0;
+            return _errorStringBuilder;
+        }
+    }
+
+}
